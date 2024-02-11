e89933fd2fbe8ee37d5bcf4926f55e4d7dea6937
Ahlam Alsakkaf <157818610+Ahlam2024@users.noreply.github.com>
2024-02-11 05:10:07 -0800

    Add files via upload

---
+++ Advanced JS2/Untitled-1.js
@@ -0,0 +1,35 @@
+//_____Array Ex_____
+//ex1
+//let  arr = [5, 3, 8, 1]
+//let newArr= arr.filter(e=> e>0 && e<5)
+//console.log(newArr)
+//console.log(arr)
+
+//ex2
+//let adam = { name: "adam", age: 30 }
+//let amal = { name: "amal", age: 91 }
+//let sama = { name: "sama", age: 35 }
+//
+//let users = [adam, amal, sama]
+//
+//let names = users.map(e=>e=e.name)
+// console.log(names)
+//   alert( names )
+
+//ex3
+
+//let dean ={name: "dean", ag: 23}
+//let kate ={name: "kate", age:32}
+//let jame ={name: "james", age:65}
+//let users = [dean, kate, jame]
+//
+//function getAverageAge(users){
+//   if(users.length===0){
+//        return 0
+//    }
+//    let sumAge=users.reduce((sum ,user)=> sum + user.age, 0);
+//    let ave= sumAge / users.length;
+//    return ave
+//
+//}
+//console.log(getAverageAge(users))
